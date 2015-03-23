23b2a14e294e01b4bb7bb9e3cdc3ce6a4638bc87
unicodesnowman <utfsnowman@gmail.com>
2015-03-23 14:00:29 -0400

    Add basic setup script

---
+++ setup.sh
@@ -0,0 +1,31 @@
+# clone and set up dotfiles
+#
+
+if [ ! -L ~/.dotfiles ]; then
+    echo "Setting up dotfiles..."
+    git clone https://github.com/UnicodeSnowman/dotfiles.git ~/.dotfiles
+    ~/.dotfiles/setup.sh
+else
+    echo ".dotfiles directory exists!"
+fi
+
+if [ ! -L ~/.vim ]; then
+    echo "Setting up vim files..."
+    git clone https://github.com/UnicodeSnowman/cap-vim.git ~/.vim
+    ~/.vim/setup.sh
+else
+    echo ".vim directory exists!"
+fi
+
+
+if type brew; then
+    echo "you gots brew"
+    brew install ansible
+    # brew install git bash-completion - do this via ansible
+    # brew install tmux - do this via ansible
+    # brew install vim - do this via ansible
+else
+    echo "aint gots the brew, we can fix that..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
