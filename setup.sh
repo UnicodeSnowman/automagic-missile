88801beae2cb8b01c7568c94fd44ad9a4a5d3946
unicodesnowman <utfsnowman@gmail.com>
2015-03-23 22:14:13 -0400

    updates

--- setup.sh
+++ setup.sh
@@ -6,25 +6,5 @@ if type brew; then
 else
     echo "aint gots the brew, we can fix that..."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    echo "Homebrew installed. please run the setup script again"
+    echo "If Homebrew was successfully installed, please run the setup script again"
 fi
-
-# clone and set up dotfiles
-
-#if [ ! -d ~/.dotfiles ]; then
-#    echo "Setting up dotfiles..."
-#    git clone https://github.com/UnicodeSnowman/dotfiles.git ~/.dotfiles
-#    ~/.dotfiles/setup.sh
-#else
-#    echo ".dotfiles directory exists!"
-#fi
-#
-#if [ ! -d ~/.vim ]; then
-#    echo "Setting up vim files..."
-#    git clone https://github.com/UnicodeSnowman/cap-vim.git ~/.vim
-#    ~/.vim/setup.sh
-#else
-#    echo ".vim directory exists!"
-#fi
-#
-#
