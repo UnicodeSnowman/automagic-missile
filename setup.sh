d8a039cccb2a4bee7d1befa5526e6fcce4ef5a33
unicodesnowman <utfsnowman@gmail.com>
2015-03-23 22:12:37 -0400

    more automation

--- setup.sh
+++ setup.sh
@@ -1,22 +1,3 @@
-# clone and set up dotfiles
-
-if [ ! -d ~/.dotfiles ]; then
-    echo "Setting up dotfiles..."
-    git clone https://github.com/UnicodeSnowman/dotfiles.git ~/.dotfiles
-    ~/.dotfiles/setup.sh
-else
-    echo ".dotfiles directory exists!"
-fi
-
-if [ ! -d ~/.vim ]; then
-    echo "Setting up vim files..."
-    git clone https://github.com/UnicodeSnowman/cap-vim.git ~/.vim
-    ~/.vim/setup.sh
-else
-    echo ".vim directory exists!"
-fi
-
-
 if type brew; then
     echo "you gots brew"
     brew install ansible
@@ -28,3 +9,22 @@ else
     echo "Homebrew installed. please run the setup script again"
 fi
 
+# clone and set up dotfiles
+
+#if [ ! -d ~/.dotfiles ]; then
+#    echo "Setting up dotfiles..."
+#    git clone https://github.com/UnicodeSnowman/dotfiles.git ~/.dotfiles
+#    ~/.dotfiles/setup.sh
+#else
+#    echo ".dotfiles directory exists!"
+#fi
+#
+#if [ ! -d ~/.vim ]; then
+#    echo "Setting up vim files..."
+#    git clone https://github.com/UnicodeSnowman/cap-vim.git ~/.vim
+#    ~/.vim/setup.sh
+#else
+#    echo ".vim directory exists!"
+#fi
+#
+#
