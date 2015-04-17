37c0e08789b9750ea6ab3fd1ed71fc50b1b13667
unicodesnowman <utfsnowman@gmail.com>
2015-04-17 09:56:05 -0400

    Better setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
-if type brew; then
-    echo "you gots brew"
-    brew install ansible
-    brew tap phinze/homebrew-cask
-    brew install brew-cask
-else
+if ! type brew; then
     echo "aint gots the brew, we can fix that..."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    echo "If Homebrew was successfully installed, please run the setup script again"
 fi
+
+if ! type ansible; then
+    echo "Installing ansible..."
+    brew install ansible
+fi
+
+if ! type brew-cask; then
+    echo "Installing homebrew-cask..."
+    brew tap phinze/homebrew-cask
+    brew install brew-cask
+fi
+
+echo "* fin *"
