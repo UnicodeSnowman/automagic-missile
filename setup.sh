b0cfa22507e4a75237e43f70307d820587d9bd65
Christopher Caplinger <utfsnowman@gmail.com>
2017-07-09 18:41:49 -0400

    Add caskroom fonts and versions

--- setup.sh
+++ setup.sh
@@ -13,6 +13,8 @@ fi
 if ! type brew-cask; then
     echo "Installing homebrew-cask..."
     brew tap caskroom/cask
+    brew tap caskroom/versions
+    brew tap caskroom/fonts
     brew install brew-cask
 fi
 
